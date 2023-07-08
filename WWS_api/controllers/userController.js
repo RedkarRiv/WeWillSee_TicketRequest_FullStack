dec240a59205a569ab05cc795997a8c792ee9f8d
Carlos <carresan.val@gmail.com>
2023-07-08 20:10:27 +0200

    UPDATE: Añadido userController.getAll

--- WWS_api/controllers/userController.js
+++ WWS_api/controllers/userController.js
@@ -23,4 +23,25 @@ userController.getOne = async (req, res) => {
   }
 };
 
+
+userController.getAll = async (req, res) => {
+    try {
+      const allUser = await User.findAll({
+        attributes: { exclude: ["password"] },
+      });
+  
+      return res.json({
+        success: true,
+        message: "Datos de todos los usuarios recuperados",
+        data: allUser,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Los datos no han podido ser recuperados",
+        error: error.message,
+      });
+    }
+  };
+
 module.exports = userController;
--- WWS_api/views/userRoutes.js
+++ WWS_api/views/userRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-
 const userController = require('../controllers/userController');
+const isAdmin = require("../middleware/isAdmin");
 const auth = require("../middleware/tokenVerify");
 
 router.get('/myprofile', auth,  userController.getOne);
+router.get('/all', auth, isAdmin,  userController.getAll);
 
 module.exports = router;
