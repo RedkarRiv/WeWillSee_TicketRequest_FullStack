13e0c443e6cf3526adef2ac1131f83d924c29a9d
Carlos <carresan.val@gmail.com>
2023-07-11 14:56:38 +0200

    UPDATE: Añadido userController.activateOne y corregido userController.inactivateOne para que no solicite params y el criterio sea el token

--- WWS_api/controllers/userController.js
+++ WWS_api/controllers/userController.js
@@ -211,4 +211,37 @@ userController.inactivateOne = async (req, res) => {
   }
 };
 
+userController.activateOne = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const userCheck = await User.findByPk(userId);
+
+    if (!userCheck) {
+      return res.json({
+        success: true,
+        message: "El usuario no existe",
+      });
+    }
+    const inactivateUser = await User.update(
+      { user_status: "Activate" },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    return res.json({
+      success: true,
+      message: "El usuario ha sido activado",
+      data: inactivateUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "El usuario no ha podido ser activado",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = userController;
--- WWS_api/views/userRoutes.js
+++ WWS_api/views/userRoutes.js
@@ -5,7 +5,8 @@ const auth = require("../middleware/tokenVerify");
 
 router.get('/myprofile', auth,  userController.getOne);
 router.post('/update', auth,  userController.updateUser);
-router.get('/inactivate/:id', auth,  userController.inactivateOne);
+router.get('/inactivate', auth,  userController.inactivateOne);
+router.get('/activate', auth,  userController.activateOne);
 router.get('/all', auth, isAdmin,  userController.getAll);
 router.delete('/destroy/:id', auth, isAdmin,  userController.deleteOne);
 router.get('/all/filter', auth, isAdmin, userController.getAll);
