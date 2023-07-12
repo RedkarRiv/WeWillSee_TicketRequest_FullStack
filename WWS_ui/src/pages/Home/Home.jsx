b8beb828975d4ea1129edb838db6ea0494b55160
Carlos <carresan.val@gmail.com>
2023-07-12 21:48:24 +0200

    UPDATE: Añadido boton estatico al Home para diseño de credencialSlide

--- WWS_ui/src/pages/Home/Home.jsx
+++ WWS_ui/src/pages/Home/Home.jsx
@@ -7,13 +7,14 @@ export const Home = () => {
   return (
     <div className="homeBackground">
       <div className="credentialSlide">
-        <div className="logoContainer mb-5">
+        <div className="logoContainer mb-3">
         </div>
         <LoginCard />
         <div className="forgottPassword mt-1 mb-1">
           ¿No tienes cuenta todavía?
         </div>
         <div className="notAccountRegist mt-1 mb-1"> Registrate</div>
+        <div className="buttonLogin  mt-3">Enviar</div>
       </div>
 
       <div className="imageSlide"></div>
