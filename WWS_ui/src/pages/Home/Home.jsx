204cff017d1c56ea5c03020a7b4760601ac9ad50
Carlos <carresan.val@gmail.com>
2023-07-12 20:22:28 +0200

    UPDATE: Formateado Home. Creado y formateado Logo. Incluida imagen background en Home

--- WWS_ui/src/pages/Home/Home.jsx
+++ WWS_ui/src/pages/Home/Home.jsx
@@ -7,7 +7,13 @@ export const Home = () => {
   return (
     <div className="homeBackground">
       <div className="credentialSlide">
+        <div className="logoContainer mb-5">
+        </div>
         <LoginCard />
+        <div className="forgottPassword mt-1 mb-1">
+          ¿No tienes cuenta todavía?
+        </div>
+        <div className="notAccountRegist mt-1 mb-1"> Registrate</div>
       </div>
 
       <div className="imageSlide"></div>
