7a968888fb933e9babe82dc3fba970062e1b940b
Carlos <carresan.val@gmail.com>
2023-07-12 10:02:09 +0200

    UPDATE: Cambios menores de limpieza y reestructuración de código

--- WWS_api/router.js
+++ WWS_api/router.js
@@ -4,6 +4,7 @@ const userRoutes = require("./views/userRoutes")
 const adminRoutes = require("./views/adminRoutes")
 
 
+
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/admin", adminRoutes);
