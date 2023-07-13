0deb183dedc7ef483e49e8f6409d071ee00853fb
Carlos <carresan.val@gmail.com>
2023-07-13 13:20:08 +0200

    UPDATE: Corregido diseño responsive de Home_desktop y Home_mobile. Incluido y formateado avatar de usuario

--- WWS_ui/src/pages/Dashboard/Dashboard.jsx
+++ WWS_ui/src/pages/Dashboard/Dashboard.jsx
@@ -6,10 +6,23 @@ import { Col, Container, Row } from "react-bootstrap";
 export const Dashboard = () => {
   return (
     <Container fluid className="dashboardBox p-0 m-0 d-flex">
-      <Row className="dashboardBackground d-flex flex-column flex-md-row justify-content-center align-content-center p-1 m-0">
-        <Col className="profileBar col-md-2 col-sm-12 m-1"></Col>
-        <Col className="dashboardContainer col-md-9 col-sm-12 m-1"></Col>
+      <Row className="dashboardBackground d-flex flex-column flex-md-row justify-content-center align-items-center px-1 m-0">
+        <Col className="profileBar col-md-2 col-sm-12 my-1 mx-md-1 align-items-center">
+          <div className="profileAvatarContainer d-flex m-0 pt-md-4 justify-content-md-center align-items-center justify-content-sm-start">
+            <div className=" m-0">
+                <img
+                fluid
+                src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+                alt="Avatar logo"
+                className= "profileAvatar"
+                />
+            </div>
+          </div>
+          <div className="profileButtons"></div>
+        </Col>
+        <Col className="dashboardContainer col-md-9 col-sm-12 my-1">
+        </Col>
       </Row>
-    </Container>
+    </Container>    
   );
 };
