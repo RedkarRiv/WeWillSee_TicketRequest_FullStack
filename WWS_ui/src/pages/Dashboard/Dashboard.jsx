8bd6991cc212739248d202e5a5e315d7ee60dca7
Carlos <carresan.val@gmail.com>
2023-07-13 14:09:36 +0200

    FIX: Ajustes menores de diseño

--- WWS_ui/src/pages/Dashboard/Dashboard.jsx
+++ WWS_ui/src/pages/Dashboard/Dashboard.jsx
@@ -7,10 +7,9 @@ export const Dashboard = () => {
   return (
     <Container fluid className="dashboardBox p-0 m-0 d-flex">
       <Row className="dashboardBackground d-flex flex-column flex-md-row justify-content-center align-items-center px-1 m-0">
-        <Col className="profileBar col-md-2 col-sm-12 my-1 mx-md-1  d-flex align-items-sm-center justify-content-sm-start align-items-md-start justify-content-md-center">
-
-          <div className="profileAvatarContainer d-flex  flex-column m-0 pt-md-4 justify-content-md-center align-items-center justify-content-center">
-            <div className="m-0 d-flex">
+        <Col className="profileBar col-md-2 col-sm-12 my-1 mx-md-1 d-flex flex-md-column flex-sm-row">
+          <div className="profileAvatarContainer d-flex  flex-column  m-0 pt-md-3 align-items-sm-center justify-content-sm-center align-items-md-center justify-content-md-start">
+            <div className="m-0 d-flex p-1 justify-content-center align-items-center"> 
                 <img
                 fluid
                 src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
@@ -18,10 +17,16 @@ export const Dashboard = () => {
                 className= "profileAvatar"
                 />
             </div>
-            <div className="userTitle p-2xp">Usuario Usuario</div>
+            <div className="userTitle pt-md-2">Requester Name</div>
+            <div className="logoutDesign">Logout</div>
 
           </div>
-          <div className="profileButtons"></div>
+          <div className="profileButtons d-flex flex-column justify-content-center align-items-center pt-md-3">
+            <div className="buttonDesign">Mi perfil</div>
+            <div className="buttonDesign">Mis tickets</div>
+            <div className="buttonDesignNew">New ticket</div>
+          </div>
+          
         </Col>
         <Col className="dashboardContainer col-md-9 col-sm-12 my-1">
         </Col>
