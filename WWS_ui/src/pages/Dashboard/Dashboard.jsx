ffd5c11ec6c432d474330049c7fa9e0407010a57
Carlos <carresan.val@gmail.com>
2023-07-13 13:30:19 +0200

    UPDATE: Añadido nombre de usuario y formateado en version movil y desktop

--- WWS_ui/src/pages/Dashboard/Dashboard.jsx
+++ WWS_ui/src/pages/Dashboard/Dashboard.jsx
@@ -7,9 +7,10 @@ export const Dashboard = () => {
   return (
     <Container fluid className="dashboardBox p-0 m-0 d-flex">
       <Row className="dashboardBackground d-flex flex-column flex-md-row justify-content-center align-items-center px-1 m-0">
-        <Col className="profileBar col-md-2 col-sm-12 my-1 mx-md-1 align-items-center">
-          <div className="profileAvatarContainer d-flex m-0 pt-md-4 justify-content-md-center align-items-center justify-content-sm-start">
-            <div className=" m-0">
+        <Col className="profileBar col-md-2 col-sm-12 my-1 mx-md-1  d-flex align-items-sm-center justify-content-sm-start align-items-md-start justify-content-md-center">
+
+          <div className="profileAvatarContainer d-flex  flex-column m-0 pt-md-4 justify-content-md-center align-items-center justify-content-center">
+            <div className="m-0 d-flex">
                 <img
                 fluid
                 src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
@@ -17,6 +18,8 @@ export const Dashboard = () => {
                 className= "profileAvatar"
                 />
             </div>
+            <div className="userTitle p-2xp">Usuario Usuario</div>
+
           </div>
           <div className="profileButtons"></div>
         </Col>
