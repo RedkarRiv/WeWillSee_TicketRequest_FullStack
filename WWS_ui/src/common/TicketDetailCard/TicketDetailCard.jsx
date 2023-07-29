d4f6653ecec61239ecffb516b7406145eafa587e
Carlos <carresan.val@gmail.com>
2023-07-29 19:08:56 +0200

    FIX: Corregido error por el cual se mostraba el contenido del nuevo comentario en el input aunque cerraras el modal o cambiaras de ticket

--- WWS_ui/src/common/TicketDetailCard/TicketDetailCard.jsx
+++ WWS_ui/src/common/TicketDetailCard/TicketDetailCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MDBCard, MDBCardBody, MDBRow, MDBCol, MDBBtn } from "mdb-react-ui-kit";
 import "./TicketDetailCard.css";
 import { useSelector } from "react-redux";
@@ -8,32 +8,21 @@ import { TitleSectionCard } from "../TitleSectionCard/TitleSectionCard";
 import { CommentCard } from "../CommentCard/CommentCard";
 
 export const TicketDetailCard = ({ ticket, onClose }) => {
-  const [credentialsError, setCredentialsError] = useState({
-    title: "",
-    description: "",
-  });
-  const InputCheck = (e) => {
-    console.log(credentialsError);
-    let mensajeError = CheckError(e.target.name, e.target.value);
-
-    setCredentialsError((prevState) => ({
-      ...prevState,
-      [e.target.name + "Error"]: mensajeError,
-    }));
-  };
-  console.log(ticket);
+  const [closeComment, setCloseComment] = useState(false);
   const credentialsRdx = useSelector(userDataCheck);
-  console.log(credentialsRdx);
 
   const roleCheck = credentialsRdx.credentials.user.roleId;
-  const credentialCheck = credentialsRdx?.credentials?.token;
   console.log(roleCheck);
   const [newComment, setNewComment] = useState("");
+  console.log(newComment);
+
   const InputHandler = (e) => {
-    setNewComment((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
+    setNewComment(e.target.value);
+  };
+
+  const resetCommentHandler = () => {
+    setNewComment("");
+    onClose();
   };
 
   return (
@@ -41,10 +30,10 @@ export const TicketDetailCard = ({ ticket, onClose }) => {
       <MDBCol className="p-0 m-0">
         <MDBCard className="d-flex justify-content-center align-items-center cardDetailBackgroundDesign">
           <MDBRow className="p-0 m-0 d-flex justify-content-end align-items-center w-100">
-            <div  
+            <div
               color="secondary"
               className="closeButtonModal"
-              onClick={onClose}
+              onClick={() => resetCommentHandler()}
             >
               X
             </div>
@@ -55,41 +44,65 @@ export const TicketDetailCard = ({ ticket, onClose }) => {
               <MDBCardBody className="d-flex flex-column justify-content-center mt-2">
                 <MDBRow>
                   <MDBCol md="12" className="mt-4">
-                    <div className="inputTicketDetail">
-                      {ticket?.Category?.Theme?.theme_name}
-                    </div>
+                    <MDBRow className="inputTicketDetail d-flex m-0">
+                      <MDBCol className="categoryLabel px-2">Tema</MDBCol>
+                      <MDBCol className="contentLabel px-2">
+                        {" "}
+                        {ticket?.Category?.Theme?.theme_name}
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                 </MDBRow>
 
                 <MDBRow>
                   <MDBCol md="12" className="mt-3">
-                    <div className="inputTicketDetail">
-                      {ticket?.Category?.category_name}
-                    </div>
+                    <MDBRow className="inputTicketDetail d-flex m-0">
+                      <MDBCol className="categoryLabel px-2">Categoría</MDBCol>
+                      <MDBCol className="contentLabel px-2">
+                        {" "}
+                        {ticket?.Category?.category_name}
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                 </MDBRow>
 
                 <MDBRow>
                   <MDBCol md="12" className="mt-3">
-                    <div className="inputTicketDetail">
-                      {ticket?.ticket_title}
-                    </div>
+                    <MDBRow className="inputTicketDetail d-flex m-0">
+                      <MDBCol className="categoryLabel px-2">Título</MDBCol>
+                      <MDBCol className="contentLabel px-2">
+                        {" "}
+                        {ticket?.ticket_title}
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                 </MDBRow>
 
                 <MDBRow>
                   <MDBCol md="12" className="mt-3">
-                    <div className="inputTicketDetail p-3">
-                      {ticket?.ticket_description}
-                    </div>
+                    <MDBRow className="inputTicketDetail d-flex m-0">
+                      <MDBCol className="categoryLabel px-2 col-12">
+                        Descripción
+                      </MDBCol>
+                      <MDBCol className="contentLabelDescription px-2 py-2 col-12">
+                        {" "}
+                        {ticket?.ticket_description}
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                 </MDBRow>
 
                 <MDBRow>
                   <MDBCol md="12" className="mt-3">
-                    <div className="inputTicketDetail">
-                      {ticket?.SAT?.User?.name}
-                    </div>
+                    <MDBRow className="inputTicketDetail d-flex m-0">
+                      <MDBCol className="categoryLabel px-2">
+                        SAT asignado
+                      </MDBCol>
+                      <MDBCol className="contentLabel pt-2">
+                        {" "}
+                        {ticket?.SAT?.User?.name}
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                 </MDBRow>
 
@@ -99,17 +112,21 @@ export const TicketDetailCard = ({ ticket, onClose }) => {
                       COMENTARIOS
                     </div>
                   </MDBCol>
-                  {ticket.Messages
-                    ? Object.values(ticket.Messages).map((comment, index) => (
-                        <MDBCol md="12" className="mt-0" key={index}>
-                          <CommentCard
-                            user={comment?.User?.name}
-                            date={comment?.updatedAt}
-                            comment={comment?.message_content}
-                          />{" "}
-                        </MDBCol>
-                      ))
-                    : "NO HAY COMENTARIOS"}
+                  {ticket.Messages.length !== 0 ? (
+                    Object.values(ticket.Messages).map((comment, index) => (
+                      <MDBCol md="12" className="mt-0" key={index}>
+                        <CommentCard
+                          user={comment?.User?.name}
+                          date={comment?.updatedAt}
+                          comment={comment?.message_content}
+                        />{" "}
+                      </MDBCol>
+                    ))
+                  ) : (
+                    <div className="noComments w-100 d-flex justify-content-center">
+                      NO HAY COMENTARIOS
+                    </div>
+                  )}
                 </MDBRow>
                 <MDBRow>
                   <MDBCol md="12" className="mt-3">
@@ -117,11 +134,10 @@ export const TicketDetailCard = ({ ticket, onClose }) => {
                       type="textarea"
                       placeholder="Nuevo comentario"
                       name="comment"
-                      contentEditable
+                      value={newComment}
                       maxLength={500}
                       className="commentDesign"
                       onChange={(e) => InputHandler(e)}
-                      onBlur={(e) => InputCheck(e)}
                     />
                   </MDBCol>
                 </MDBRow>
@@ -150,7 +166,7 @@ export const TicketDetailCard = ({ ticket, onClose }) => {
                     >
                       Reasignar
                     </div>
-                  ): null}
+                  ) : null}
                 </MDBRow>
               </MDBCardBody>
             </MDBCol>
--- WWS_ui/src/common/TicketListCard/TicketListCard.jsx
+++ WWS_ui/src/common/TicketListCard/TicketListCard.jsx
@@ -107,6 +107,7 @@ export const TicketListCard = () => {
   };
   const handleCloseModal = () => {
     setShowModal(false);
+    
   };
 
   useEffect(() => {
