5031555471d26e52bea7006851d7f09562ea1f7d
Carlos <carresan.val@gmail.com>
2023-07-31 02:30:56 +0200

    FIX: Reajustado diseño de page Message en version movil

--- WWS_ui/src/pages/Message/Message.jsx
+++ WWS_ui/src/pages/Message/Message.jsx
@@ -20,15 +20,15 @@ export const Message = () => {
   return (
     <Container fluid className="p-0 m-0 d-flex">
       <Row className="dashboardBackground d-flex flex-md-row justify-content-center align-items-start px-1 m-0 pt-md-5">
-        <Col className="profileBar col-md-8 col-sm-12 d-flex flex-column justify-content-center pt-md-4 pb-md-4">
-          <div className="w-100 d-flex mb-5 justify-content-center">
+        <Col className="messageContainerDesign col-md-8 col-sm-12 d-flex flex-column justify-content-center pt-md-4 pb-md-4">
+          <div className="w-100 d-flex mb-md-5 justify-content-center">
           <img
                 src={logoBackWWS}
                 alt="Avatar logo"
                 className="logoMessageDesign"
               />
           </div>
-          <div className="messageTextDesign text-center mb-3"> {message}</div>
+          <div className="messageTextDesign text-center mb-md-3 mt-3"> {message}</div>
         </Col>
       </Row>
     </Container>
