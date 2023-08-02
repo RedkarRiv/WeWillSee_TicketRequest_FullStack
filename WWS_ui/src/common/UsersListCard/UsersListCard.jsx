f2f0b3d1810a097196920cffc7b22fee499cb21c
Carlos <carresan.val@gmail.com>
2023-08-02 17:53:21 +0200

    TEST: Corrección de filtro de usuarios por role v2

--- WWS_ui/src/common/UsersListCard/UsersListCard.jsx
+++ WWS_ui/src/common/UsersListCard/UsersListCard.jsx
@@ -24,8 +24,7 @@ export const UsersListCard = () => {
   const [usersData, setUsersData] = useState([]);
   const [activeComponentView, setActiveComponentView] = useState(1);
   const inputRef = useRef(null);
-  console.log("esto es usersData")
-console.log(usersData)
+
   const [criteria, setCriteria] = useState(null);
   const [userSelected, setUserSelected] = useState({});
   const [showModal, setShowModal] = useState(false);
@@ -45,11 +44,6 @@ console.log(usersData)
       name: "Por mail  ",
       value: "mail",
       fieldName: "email",
-    },
-    {
-      name: "Por role  ",
-      value: "string",
-      fieldName: "role_id",
     },
     {
       name: "Por estado  ",
@@ -161,11 +155,6 @@ console.log(usersData)
             </Dropdown>
           </div>
         </MDBCol>
-
-
-
-
-
         <MDBCol className="col-7 d-flex justify-content-start p-0 m-0">
           <input
             className="inputSearchOptions h-100 w-100"
@@ -176,8 +165,6 @@ console.log(usersData)
             ref={inputRef}
           />
         </MDBCol>
-
-
         <MDBCol className="col-2 col-xxl-1 p-1 d-flex justify-content-center align-items-center">
           <div
             className="deleteFiltersData d-flex justify-content-center"
@@ -186,9 +173,6 @@ console.log(usersData)
             Reset
           </div>
         </MDBCol>
-
-
-
       </MDBRow>
       <MDBTable align="middle">
         <MDBTableHead>
